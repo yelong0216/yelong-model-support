8e53c8fce6660a5240ef6c4b7c5a19a123e8f71b
YL <yl1430834495@163.com>
2020-09-24 14:09:31 +0800

    发布2.1.3版本

--- src/main/java/org/yelong/core/model/support/generator/GWrapperModelAndTableWrapper.java
+++ src/main/java/org/yelong/core/model/support/generator/GWrapperModelAndTableWrapper.java
@@ -1,11 +1,12 @@
 package org.yelong.core.model.support.generator;
 
 import org.yelong.core.model.manage.ModelAndTable;
+import org.yelong.core.model.manage.wrapper.ModelAndTableWrapper;
 
 /**
  * @since 2.0
  */
-public class GWrapperModelAndTableWrapper extends GModelAndTableWrapper implements GModelAndTable {
+public class GWrapperModelAndTableWrapper extends ModelAndTableWrapper implements GModelAndTable {
 
 	private final GModelAndTable gModelAndTable;
 
@@ -16,8 +17,48 @@ public class GWrapperModelAndTableWrapper extends GModelAndTableWrapper implemen
 	}
 
 	@Override
-	public GModelAndTable getgModelAndTable() {
-		return gModelAndTable;
+	public String getModelClassName() {
+		return gModelAndTable.getModelClassName();
+	}
+
+	@Override
+	public String getModelClassSimpleName() {
+		return gModelAndTable.getModelClassSimpleName();
+	}
+
+	@Override
+	public String getModelClassPackageName() {
+		return gModelAndTable.getModelClassPackageName();
+	}
+
+	@Override
+	public String getAuthor() {
+		return gModelAndTable.getAuthor();
+	}
+
+	@Override
+	public void setAuthor(String author) {
+		gModelAndTable.setAuthor(author);
+	}
+
+	@Override
+	public String getSuperClassName() {
+		return gModelAndTable.getSuperClassName();
+	}
+
+	@Override
+	public void setSuperClassName(String superClassName) {
+		gModelAndTable.setSuperClassName(superClassName);
+	}
+
+	@Override
+	public String getSuperClassSimpleName() {
+		return gModelAndTable.getSuperClassName();
+	}
+
+	@Override
+	public void setSuperClassSimpleName(String superClassSimpleName) {
+		gModelAndTable.setSuperClassSimpleName(superClassSimpleName);
 	}
 
 }
